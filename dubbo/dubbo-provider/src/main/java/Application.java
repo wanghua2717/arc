da92a1bd2958beccdddfe977dbda7de7de9fe713
王华 <wanghua2717@126.com>
2018-04-10 11:00:14 +0800

    add spring web

--- dubbo/dubbo-provider/src/main/java/Application.java
+++ dubbo/dubbo-provider/src/main/java/Application.java
@@ -2,6 +2,7 @@ import com.wang.dubbo.provider.UserServiceImpl;
 import org.apache.log4j.Logger;
 
 public class Application {
+
     public static void main(String[] args) {
        com.alibaba.dubbo.container.Main.main(args);
 
